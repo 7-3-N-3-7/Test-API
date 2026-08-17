4853efe60f02f6617d646602a811b92661468ae6
73N37 <X0D4R4P@proton.me>
2026-08-17 20:17:01 +0200

    Add Cucumber.js and Playwright for UI E2E testing

---
+++ ui/tests/support/world.ts
@@ -0,0 +1,26 @@
+import { setWorldConstructor, World, IWorldOptions } from '@cucumber/cucumber';
+import { Browser, BrowserContext, Page, chromium } from '@playwright/test';
+
+export class CustomWorld extends World {
+  browser: Browser | null = null;
+  context: BrowserContext | null = null;
+  page: Page | null = null;
+
+  constructor(options: IWorldOptions) {
+    super(options);
+  }
+
+  async init() {
+    this.browser = await chromium.launch({ headless: true });
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+  }
+
+  async cleanup() {
+    await this.page?.close();
+    await this.context?.close();
+    await this.browser?.close();
+  }
+}
+
+setWorldConstructor(CustomWorld);
