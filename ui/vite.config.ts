4522ea32adeffa452ceb1262a312a42d12743643
73N37 <X0D4R4P@proton.me>
2026-08-17 22:22:32 +0200

    fix: add allowedHosts configuration for Vite server to internal docker container

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['host.docker.internal'],
     proxy: {
       '/api': 'http://localhost:8081',
       '/public': 'http://localhost:8081'
